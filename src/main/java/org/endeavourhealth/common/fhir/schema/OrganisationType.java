faffa1cc2f5c7a111204e56362e0930fe150732a
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-06-17 17:12:16 +0100

    Add organisation role code to OrganisationType

--- src/main/java/org/endeavourhealth/common/fhir/schema/OrganisationType.java
+++ src/main/java/org/endeavourhealth/common/fhir/schema/OrganisationType.java
@@ -4,61 +4,62 @@ import org.endeavourhealth.common.fhir.FhirValueSetUri;
 
 public enum OrganisationType {
 
-    APPLICATION_SERVICE_PROVIDER("AR", "Application Service Provider"),
-    BOOKING_MANAGEMENT_SYSTEM("BM", "Booking Management System (BMS) Call Centre Establishment"),
-    CANCER_NETWORK("CN", "Cancer Network"),
-    CANCER_REGISTRY("CR", "Cancer Registry"),
-    CARE_HOME_HQ("CQ", "Care Home Headquarters"),
-    CARE_TRUST("CT", "Care Trust"),
-    CCG("CC", "Clinical Commissioning Group (CCG)"),
-    CLINCIAL_NETWORK("CL", "Clinical Network"),
-    CSU("CA", "Commissioning Support Unit (CSU)"),
-    COURT("JG", "Court"),
-    DENTAL_PRACTICE("DD", "Dental Practice"),
-    EDUCATION_ESTABLISHMENT("ED", "Education Establishment"),
-    EXECUTIVE_AGENCY("EA", "Executive Agency"),
-    EXECUTIVE_AGENCY_PROGRAMME("AP", "Executive Agency Programme"),
-    GOVERNMENT_DEPARTMENT("GD", "Government Department"),
-    GOVERNMENT_OFFICE_REGION("GO", "Government Office Region (GOR)"),
-    ABEYANCE_AND_DISPERSALE_GP_PRACTICE("AA", "Abeyance and Dispersal GP Practice"),
-    GP_PRACTICE("PR", "GP Practices in England and Wales"),
-    HIGH_LEVEL_HEALTH_GEOGRAPHY("HA", "High Level Health Geography"),
-    IMMIGRATION_REMOVAL_CENTRE("JD", "Immigration Removal Centre"),
-    ISHP("PH", "Independent Sector Healthcare Provider (ISHP)"),
-    LOCAL_AUTHORITY("EL", "Local Authority"),
-    LOCAL_HEALTH_BOARD_WALES("LB", "Local Health Board (Wales)"),
-    LSP("LO", "Local Service Provider (LSP)"),
-    MILITARY_HOSPITAL("MH", "Military Hospital"),
-    NATIONAL_APPLICATION_SERVICE_PROVIDER("NP", "National Application Service Provider"),
-    NATIONAL_GROUPINGS("RO", "National Groupings"),
-    NHS_SUPPORT_AGENCY("NS", "NHS Support Agency"),
-    NHS_TRUST("TR", "NHS Trust"),
-    NHS_TRUST_SERVICE("TS", "NHS Trust Service"),
-    NON_NHS_ORGANISATION("NN", "Non-NHS Organisation"),
-    NI_HEALTH_AND_SOCIAL_CARE_BOARD("NA", "Northern Ireland Health & Social Care Board"),
-    NI_HEALTH_AND_SOCIAL_CARE_TRUST("NB", "Northern Ireland Health & Social Care Trust"),
-    NI_LOCAL_COMMISSIONING_GROUP("NC", "Northern Ireland Local Commissioning Group"),
-    OPTICAL_HQ("OH", "Optical Headquarters"),
-    OSX("OA", "Other Statutory Authority (OSA)"),
-    PHARMACY("PY", "Pharmacy"),
-    PHARMACY_HQ("PX", "Pharmacy Headquarters"),
-    POLICE_CONSTABULARY("JE", "Police Constabulary"),
-    POLICE_CUSTODY_SUITE("JF", "Police Custody Suite"),
-    PCT("PT", "Primary Care Trust"),
-    PRIMARY_HEALTHCARE_DIRECTORATE("ID", "Primary Healthcare Directorate (Isle of Man)"),
-    PRISON_HEALTH_SERVICE("PN", "Prison Health Service"),
-    SCHOOL("EE", "School"),
-    SECURE_CHILDRENS_HOME("JC", "Secure Children's Home"),
-    SECTURE_TRAINING_CENTRE("JB", "Secure Training Centre (STC)"),
-    SEXUAL_ASSUALT_REFERRAL_CENTRE("JH", "Sexual Assault Referral Centre (SARC)"),
-    SPECIAL_HEALTH_AUTHORITY("SA", "Special Health Authority (SpHA)"),
-    WELSH_ASSEMBLY("WA", "Welsh Assembly"),
-    WELSH_HEALTH_COMMISSION("WH", "Welsh Health Commission"),
-    WELSH_LOCAL_HEALTH_BOARD("LH", "Welsh Local Health Board"),
-    YOUNG_OFFENDERS_INSTITUTE("JA", "Young Offenders Institute");
+    APPLICATION_SERVICE_PROVIDER("AR", "Application Service Provider", "RO92"),
+    BOOKING_MANAGEMENT_SYSTEM("BM", "Booking Management System (BMS) Call Centre Establishment", null),
+    CANCER_NETWORK("CN", "Cancer Network", "RO103"),
+    CANCER_REGISTRY("CR", "Cancer Registry", "RO105"),
+    CARE_HOME_HQ("CQ", "Care Home Headquarters", "RO104"),
+    CARE_TRUST("CT", "Care Trust", "RO107"),
+    CCG("CC", "Clinical Commissioning Group (CCG)", "RO98"),
+    CLINCIAL_NETWORK("CL", "Clinical Network", "RO102"),
+    CSU("CA", "Commissioning Support Unit (CSU)", "RO213"),
+    COURT("JG", "Court", "RO235"),
+    DENTAL_PRACTICE("DD", "Dental Practice", "RO110"),
+    EDUCATION_ESTABLISHMENT("ED", "Education Establishment", "RO117"),
+    EXECUTIVE_AGENCY("EA", "Executive Agency", "RO116"),
+    EXECUTIVE_AGENCY_PROGRAMME("AP", "Executive Agency Programme", "RO91"),
+    GOVERNMENT_DEPARTMENT("GD", "Government Department", "RO126"),
+    GOVERNMENT_OFFICE_REGION("GO", "Government Office Region (GOR)", "RO128"),
+    ABEYANCE_AND_DISPERSALE_GP_PRACTICE("AA", "Abeyance and Dispersal GP Practice", null),
+    GP_PRACTICE("PR", "GP Practices in England and Wales", "RO76"),
+    HIGH_LEVEL_HEALTH_GEOGRAPHY("HA", "High Level Health Geography", null),
+    IMMIGRATION_REMOVAL_CENTRE("JD", "Immigration Removal Centre", "RO69"),
+    ISHP("PH", "Independent Sector Healthcare Provider (ISHP)", "RO172"),
+    LOCAL_AUTHORITY("EL", "Local Authority", "RO141"),
+    LOCAL_HEALTH_BOARD_WALES("LB", "Local Health Board (Wales)", "RO144"),
+    LSP("LO", "Local Service Provider (LSP)", "RO146"),
+    MILITARY_HOSPITAL("MH", "Military Hospital", "RO150"),
+    NATIONAL_APPLICATION_SERVICE_PROVIDER("NP", "National Application Service Provider", "RO159"),
+    NATIONAL_GROUPINGS("RO", "National Groupings", null),
+    NHS_SUPPORT_AGENCY("NS", "NHS Support Agency", "RO161"),
+    NHS_TRUST("TR", "NHS Trust", "RO197"),
+    NHS_TRUST_SERVICE("TS", "NHS Trust Service", null),
+    NON_NHS_ORGANISATION("NN", "Non-NHS Organisation", "RO157"),
+    NI_HEALTH_AND_SOCIAL_CARE_BOARD("NA", "Northern Ireland Health & Social Care Board", null),
+    NI_HEALTH_AND_SOCIAL_CARE_TRUST("NB", "Northern Ireland Health & Social Care Trust", null),
+    NI_LOCAL_COMMISSIONING_GROUP("NC", "Northern Ireland Local Commissioning Group", null),
+    OPTICAL_HQ("OH", "Optical Headquarters", "RO166"),
+    OSX("OA", "Other Statutory Authority (OSA)", "RO162"),
+    PHARMACY("PY", "Pharmacy", "RO182"),
+    PHARMACY_HQ("PX", "Pharmacy Headquarters", "RO181"),
+    POLICE_CONSTABULARY("JE", "Police Constabulary", "RO233"),
+    POLICE_CUSTODY_SUITE("JF", "Police Custody Suite", "RO234"),
+    PCT("PT", "Primary Care Trust", "RO179"),
+    PRIMARY_HEALTHCARE_DIRECTORATE("ID", "Primary Healthcare Directorate (Isle of Man)", "RO137"),
+    PRISON_HEALTH_SERVICE("PN", "Prison Health Service", "RO175"),
+    SCHOOL("EE", "School", "RO117"),
+    SECURE_CHILDRENS_HOME("JC", "Secure Children's Home", "RO231"),
+    SECTURE_TRAINING_CENTRE("JB", "Secure Training Centre (STC)", "RO230"),
+    SEXUAL_ASSUALT_REFERRAL_CENTRE("JH", "Sexual Assault Referral Centre (SARC)", "RO236"),
+    SPECIAL_HEALTH_AUTHORITY("SA", "Special Health Authority (SpHA)", "RO189"),
+    WELSH_ASSEMBLY("WA", "Welsh Assembly", "RO200"),
+    WELSH_HEALTH_COMMISSION("WH", "Welsh Health Commission", null),
+    WELSH_LOCAL_HEALTH_BOARD("LH", "Welsh Local Health Board", "RO144"),
+    YOUNG_OFFENDERS_INSTITUTE("JA", "Young Offenders Institute", "RO228");
 
     private String code = null;
     private String description = null;
+    private String roleCode = null;
 
     public String getCode() {
         return code;
@@ -68,13 +69,16 @@ public enum OrganisationType {
         return description;
     }
 
+    public String getRoleCode() { return roleCode; }
+
     public String getSystem() {
         return FhirValueSetUri.VALUE_SET_ORGANISATION_TYPE;
     }
 
-    OrganisationType(String code, String description) {
+    OrganisationType(String code, String description, String roleCode) {
         this.code = code;
         this.description = description;
+        this.roleCode = roleCode;
     }
 
     public static OrganisationType fromCode(String v) {
@@ -83,6 +87,15 @@ public enum OrganisationType {
                 return c;
 
         throw new IllegalArgumentException(v);
+    }
+
+    public static OrganisationType fromRoleCode(String roleCode) {
+        for (OrganisationType organisationType : OrganisationType.values())
+            if (organisationType.roleCode != null)
+                if (organisationType.roleCode.equalsIgnoreCase(roleCode))
+                    return organisationType;
+
+        throw new IllegalArgumentException(roleCode);
     }
 
     public static OrganisationType fromDescription(String v) {
