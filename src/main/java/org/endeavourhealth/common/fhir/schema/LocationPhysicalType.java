7483e60a09c0b9ba8b8cde2171f0d0666c595b86
eaiportal <pholm@eaiportal.com>
2018-02-22 11:30:07 +0000

    New file. Added new extended LocationPhysicalType (used in Location resource)

---
+++ src/main/java/org/endeavourhealth/common/fhir/schema/LocationPhysicalType.java
@@ -0,0 +1,52 @@
+package org.endeavourhealth.common.fhir.schema;
+
+import org.endeavourhealth.common.fhir.FhirValueSetUri;
+
+public enum LocationPhysicalType {
+
+    AMBULATORY("am", "Ambulatory"),
+    AREA("area", "Area"),
+    BED("bd", "Bed"),
+    BUILDING("bu", "Building"),
+    CABINET("ca", "Cabinet"),
+    CORRIDOR("co", "Corridor"),
+    FACILITY("fa", "Facility"),
+    JURISDICTION("jdn", "Jurisdiction"),
+    HOUSE("ho", "House"),
+    LEVEL("lvl", "Level"),
+    NURSEUNIT("nu", "Nurseunit"),
+    ROAD("rd", "Road"),
+    ROOM("ro", "Room"),
+    VEHICLE("ve", "Vehicle"),
+    WING("wi", "Wing");
+
+
+    private String code = null;
+    private String description = null;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSystem() {
+        return FhirValueSetUri.VALUE_SET_PHYSICAL_LOCATION_TYPE;
+    }
+
+    LocationPhysicalType(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static LocationPhysicalType fromCode(String v) {
+        for (LocationPhysicalType c: LocationPhysicalType.values()) {
+            if (c.getCode().equalsIgnoreCase(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+}
