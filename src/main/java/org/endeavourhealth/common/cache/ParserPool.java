1741963e322afd713de2f1e27800e22568b3f3e5
Drew Littler <drewlittler@hotmail.com>
2018-07-04 18:24:09 +0100

    Fixed memory leak in JSON serialisation of FHIR resources

--- src/main/java/org/endeavourhealth/common/cache/ParserPool.java
+++ src/main/java/org/endeavourhealth/common/cache/ParserPool.java
@@ -9,103 +9,129 @@ import org.hl7.fhir.instance.model.Type;
 import java.util.Stack;
 
 public class ParserPool implements ICache {
-	private static ParserPool instance;
+    private static ParserPool instance;
 
-	public static synchronized ParserPool getInstance() {
-		if (instance == null) {
-			instance = new ParserPool();
-			CacheManager.registerCache(instance);
-		}
-		return instance;
-	}
+    public static synchronized ParserPool getInstance() {
+        if (instance == null) {
+            instance = new ParserPool();
+            CacheManager.registerCache(instance);
+        }
+        return instance;
+    }
 
-	private final Stack<IParser> jsonPool = new Stack<>();
-	private final Stack<IParser> xmlPool = new Stack<>();
+    private final Stack<IParser> jsonPool = new Stack<>();
+    private final Stack<IParser> xmlPool = new Stack<>();
 
-	@Override
-	public String getName() { return "ParserPool"; }
+    @Override
+    public String getName() {
+        return "ParserPool";
+    }
 
-	@Override
-	public long getSize() { return jsonPool.size() + xmlPool.size(); }
+    @Override
+    public long getSize() {
+        return jsonPool.size() + xmlPool.size();
+    }
 
-	@Override
-	public synchronized void clearCache() {
-		jsonPool.clear();
-		xmlPool.clear();
-	}
+    @Override
+    public synchronized void clearCache() {
+        jsonPool.clear();
+        xmlPool.clear();
+    }
 
-	public String composeString(Resource resource) throws Exception {
-		return this.composeString(null, resource);
-	}
+    public String composeString(Resource resource) throws Exception {
+        return this.composeString(null, resource);
+    }
 
-	public String composeString(String contentType, Resource resource) throws Exception {
-		IParser parser = pop(contentType);
-		try {
-			return parser.composeString(resource);
-		} finally {
-			push(parser);
-		}
-	}
+    public String composeString(String contentType, Resource resource) throws Exception {
+        IParser parser = pop(contentType);
+        try {
+            return parser.composeString(resource);
+        } finally {
+            push(parser);
+        }
+    }
 
-	public String composeString(Type type, String contentType) throws Exception {
-		IParser parser = pop(contentType);
-		try {
-			return parser.composeString(type, contentType);
-		} finally {
-			push(parser);
-		}
-	}
+    public String composeString(Type type, String contentType) throws Exception {
+        IParser parser = pop(contentType);
+        try {
+            return parser.composeString(type, contentType);
+        } finally {
+            push(parser);
+        }
+    }
 
-	public Resource parse(String data) throws Exception {
-		return this.parse(null, data);
-	}
+    public Resource parse(String data) throws Exception {
+        return this.parse(null, data);
+    }
 
-	public Resource parse(String contentType, String data) throws Exception {
-		IParser parser = pop(contentType);
-		try {
-			return parser.parse(data);
-		} finally {
-			push(parser);
-		}
-	}
+    public Resource parse(String contentType, String data) throws Exception {
+        IParser parser = pop(contentType);
+        try {
+            return parser.parse(data);
+        } finally {
+            push(parser);
+        }
+    }
 
-	public Type parseType(String json, String contentType) throws Exception {
-		IParser parser = pop(contentType);
-		try {
-			return parser.parseType(json, contentType);
-		} finally {
-			push(parser);
-		}
-	}
+    public Type parseType(String json, String contentType) throws Exception {
+        IParser parser = pop(contentType);
+        try {
+            return parser.parseType(json, contentType);
+        } finally {
+            push(parser);
+        }
+    }
 
-	private IParser pop(String contentType) {
-		if (contentType == null || contentType.isEmpty())
-			return jsonPop();
+    private IParser pop(String contentType) {
+        if (contentType == null || contentType.isEmpty())
+            return jsonPop();
 
-		if ("text/xml".equals(contentType) || "application/xml".equals(contentType))
-			return xmlPop();
+        if ("text/xml".equals(contentType) || "application/xml".equals(contentType))
+            return xmlPop();
 
-		return jsonPop();
-	}
+        return jsonPop();
+    }
 
-	private synchronized void push(IParser parser) {
-		if (parser instanceof JsonParser)
-			jsonPool.push(parser);
-		else
-			xmlPool.push(parser);
-	}
+    private synchronized void push(IParser parser) {
+        if (parser instanceof JsonParser) {
+            //if it's one of our sub-classes, call the function to clear down the useless idMap varible which causes memory leaks
+            if (parser instanceof JsonParserCustom) {
+                ((JsonParserCustom)parser).clearIdMap();
+            }
+            jsonPool.push(parser);
 
-	private synchronized IParser jsonPop() {
-		if (jsonPool.size() > 0)
-			return jsonPool.pop();
-		else
-			return new JsonParser();
-	}
+        } else {
+            xmlPool.push(parser);
+        }
+    }
 
-	private synchronized IParser xmlPop() {
-		if (xmlPool.size() > 0)
-			return xmlPool.pop();
-		else
-			return new XmlParser();
-	}
+    private synchronized IParser jsonPop() {
+        if (jsonPool.size() > 0) {
+            return jsonPool.pop();
+
+        } else {
+            return new JsonParserCustom();
+            //return new JsonParser();
+        }
+    }
+
+    private synchronized IParser xmlPop() {
+        if (xmlPool.size() > 0)
+            return xmlPool.pop();
+        else
+            return new XmlParser();
+    }
+
+
+    /**
+     * the JsonParser class has a bug in that it builds up a map called idMap which is never
+     * used and never cleared, so as a parser is used more and more, it will grow, causing a memory leak
+     * So this sub-class simply provides a function to allow clearing that map.
+     */
+    class JsonParserCustom extends JsonParser {
+
+        public void clearIdMap() {
+            this.idMap.clear();
+        }
+    }
 }
--- src/main/java/org/endeavourhealth/common/fhir/JsonHelper.java
+++ src/main/java/org/endeavourhealth/common/fhir/JsonHelper.java
@@ -3,12 +3,13 @@ package org.endeavourhealth.common.fhir;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import org.endeavourhealth.common.cache.ParserPool;
 import org.hl7.fhir.instance.formats.JsonParser;
 import org.hl7.fhir.instance.model.Resource;
 
 public class JsonHelper {
     public static String getPrettyJson(Resource resource) throws Exception {
-        String json = new JsonParser().composeString(resource);
+        String json = ParserPool.getInstance().composeString(resource);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         com.google.gson.JsonParser jp = new com.google.gson.JsonParser();
         JsonElement je = jp.parse(json);
