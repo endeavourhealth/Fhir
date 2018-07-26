ad319e4258e76d9c298a4645544dc2e0b628f4a2
Drew Littler <drewlittler@hotmail.com>
2018-07-26 10:18:08 +0100

    New code space

--- src/main/java/org/endeavourhealth/common/fhir/FhirCodeUri.java
+++ src/main/java/org/endeavourhealth/common/fhir/FhirCodeUri.java
@@ -13,6 +13,7 @@ public class FhirCodeUri {
     public static final String CODE_SYSTEM_ICD10 = "http://hl7.org/fhir/sid/icd-10"; // see https://www.hl7.org/fhir/icd.html
     public static final String CODE_SYSTEM_OPCS4 = "http://www.endeavourhealth.org/fhir/opcs4"; //placeholder - if there is an official OPCS4 code system then please add it here and remove this comment
     public static final String CODE_SYSTEM_CERNER_CODE_ID = "http://www.endeavourhealth.org/fhir/cerner-code-id";
+    public static final String CODE_SYSTEM_UK_ED_CODE = "http://www.endeavourhealth.org/fhir/uk-ed-code";
     //NOTE: Only add CODE systems to this class (i.e. systems used in CodeableConcepts and Coding structures)
     //NOTE: Consider adding your new coding system to CodeableConceptHelper.findOriginalCode(..)
 
