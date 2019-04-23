db5b87ee4ffd2e1b60d0cf0cf31613e50193a035
Froiland Go <Froiland.Go@learning-health.co.uk>
2019-04-23 19:40:23 +0100

    Added IS_PRIMARY extension

--- src/main/java/org/endeavourhealth/common/fhir/FhirExtensionUri.java
+++ src/main/java/org/endeavourhealth/common/fhir/FhirExtensionUri.java
@@ -147,5 +147,6 @@ public class FhirExtensionUri {
     public final static String HL7_MESSAGE_TYPE = "http://endeavourhealth.org/fhir/v2-message-type-extension";
     public final static String RESOURCE_CONTEXT = "http://endeavourhealth.org/fhir/StructureDefinition/context-extension";
     public final static String PARENT_RESOURCE = "http://endeavourhealth.org/fhir/StructureDefinition/parent-resource";
+    public final static String IS_PRIMARY = "http://endeavourhealth.org/fhir/StructureDefinition/is-primary";
     //DON'T FORGET TO ADD YOUR NEW EXTENSION TO THE FHIR PROFILES (ask Drew if not sure)
 }
