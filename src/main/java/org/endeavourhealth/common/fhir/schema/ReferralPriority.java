1c370e974967658979ff5284a70546ae726ada61
Drew Littler <drewlittler@hotmail.com>
2020-05-21 13:53:17 +0100

    Changing some FHIR code constant names

--- src/main/java/org/endeavourhealth/common/fhir/schema/ReferralPriority.java
+++ src/main/java/org/endeavourhealth/common/fhir/schema/ReferralPriority.java
@@ -7,8 +7,11 @@ public enum ReferralPriority {
     ROUTINE("1", "Routine"),
     URGENT("2", "Urgent"),
     TWO_WEEK_WAIT("3", "Two Week Wait"),
-    SOON("4", "Soon");
+    SOON("4", "Soon"),
+    PRIVATE("5", "Private")
+    ;
     //NOTE: if adding new types, remember to add to any Enterprise DBs and creation scripts
+    //NOTE 2: the ORDER of these values must not be changed, since the FHIR->compass transform uses the ordinal
 
     private String code = null;
     private String description = null;
